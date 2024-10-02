7a2e3b226d821a9c8e52317543a6ad6ee7a1b884
Abdo-esse <esseabdo37@gmail.com>
2024-10-03 00:09:51 +0100

    J'ai termine la menu principal et la joute d'un reservation

---
+++ Projet-de-sas.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+
+int choix,i;
+ int reference=0;
+ int x=10;
+typedef struct{
+    int jours ;
+    int mois;
+    int annee;
+
+}ate;
+typedef struct{
+   char nom[50];
+   char prenom[50];
+   int telephone;
+   int age;
+   char statut[20];
+   int reference;
+   ate date;
+
+}data;
+data p[100];
+void saiser(){
+        printf("1-Ajouter une reservation\n");
+        printf("2-Modifier ou supprimer une reservation\n");
+        printf("3-Afficher les détails d'une reservation \n");
+        printf("4-Tri des reservations \n");
+        printf("5-Recherche des reservations\n");
+        printf("6-Statistiques\n");
+        printf("7-Quiter\n");
+        printf("Choix nombre du menu :");
+        scanf("%d",&choix);
+}
+void reservation(){
+    for( i=0;i<100;i++){
+    printf("Entrer votre nom: ");
+    scanf(" %[^\n]s",&p[i].nom);
+    printf("Entrer votre prenom: ");
+    scanf(" %[^\n]s",&p[i].prenom);
+    printf("Entrer votre N de telephon: ");
+    scanf(" %d",&p[i].telephone);
+    x++;
+    printf("Entrer la date : ");
+    printf("Entrer l'anne: ");
+    scanf(" %d",&p[i].date.annee);
+    printf("Entrer le mois: ");
+    scanf(" %d",&p[i].date.mois);
+    printf("Entrer le jour: ");
+    scanf(" %d",&p[i].date.jours);
+    break;
+    }
+}
+
+
+
+
+int main(){
+    do{
+        saiser();
+        switch (choix){
+        case 1:
+            reservation();
+            break;
+        case 2:
+            printf("en cour de modiffier");
+            break;
+        case 3:
+            printf("en cour de modiffier");
+
+            break;
+        case 4:
+            printf("en cour de modiffier");
+            break;
+        case 5:
+            printf("en cour de modiffier");
+            break;
+        case 6:
+            printf("en cour de modiffier");
+            break;
+       case 7:
+                printf("Quitter le programme\n");
+                break;
+       default:
+                printf("Le numero choisi n'est pas valide \n");
+
+
+        }
+    }while(choix != 7);
+
+
+
+    return 0;
+}
